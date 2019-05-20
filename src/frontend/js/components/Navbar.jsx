08d248aff5a3e354b07aca6e06234e30fb2b944b
Andrei Cioara <andrei@cioara.me>
2019-05-20 16:32:38 +0300

    Refactored navbar

--- src/frontend/js/components/Navbar.jsx
+++ src/frontend/js/components/Navbar.jsx
@@ -7,12 +7,12 @@ export default class _Navbar extends React.Component {
     render() {
         return (
             <Navbar bg="light" expand="sm">
-                <Navbar.Brand href="#home">Motuz</Navbar.Brand>
+                <Navbar.Brand>Motuz</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link href="#home">Dashboard</Nav.Link>
-                        <Nav.Link href="#link" disabled={true}>Copy Jobs</Nav.Link>
+                        <Nav.Link>Operations</Nav.Link>
+                        <Nav.Link disabled={true}>Copy Jobs</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
