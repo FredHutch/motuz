bc4956004eea9ade7901305e82f42b75224b339f
Andrei Cioara <andrei@cioara.me>
2019-05-20 17:00:24 +0300

    Finished C program to list dir

--- sandbox/setuid_c/setuid.c
+++ sandbox/setuid_c/setuid.c
@@ -1,6 +1,28 @@
+#include <dirent.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-    printf("Hello, World!");
+int ls(const char * path) {
+    DIR *d;
+    struct dirent *dir;
+    d = opendir(path);
+    if (d) {
+        while ((dir = readdir(d)) != NULL) {
+            printf("%s\n", dir->d_name);
+        }
+        closedir(d);
+    }
+    return(0);
+}
+
+
+
+int main(int argc, char ** argv) {
+    if (argc != 2) {
+        printf("Error This should take exctly one argument - the name of the path to read");
+        exit(1);
+    }
+
+    ls(argv[1]);
     return 0;
 }
