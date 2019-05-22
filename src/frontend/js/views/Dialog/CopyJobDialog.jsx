5d1a4714166adeb1daf4e68fa3fe91b856f77a7b
Andrei Cioara <andrei@cioara.me>
2019-05-22 09:59:27 +0300

    Whitespace

--- src/frontend/js/views/Dialog/CopyJobDialog.jsx
+++ src/frontend/js/views/Dialog/CopyJobDialog.jsx
@@ -10,18 +10,18 @@ class CopyJobDialog extends React.Component {
         return (
             <div>
                 <Modal show={true} onHide={() => this.handleClose()}>
-                  <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
-                  </Modal.Header>
-                  <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-                  <Modal.Footer>
-                    <Button variant="secondary" onClick={() => this.handleClose()}>
-                      Close
-                    </Button>
-                    <Button variant="primary" onClick={() => this.handleClose()}>
-                      Save Changes
-                    </Button>
-                  </Modal.Footer>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={() => this.handleClose()}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={() => this.handleClose()}>
+                            Save Changes
+                        </Button>
+                    </Modal.Footer>
                 </Modal>
             </div>
         );
