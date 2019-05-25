2f62b33abddacbede28cdccf3ba810b65f12cc71
Andrei Cioara <andrei@cioara.me>
2019-05-25 16:31:37 +0300

    Upgraded the copy job model

--- src/backend/api/models/copy_job.py
+++ src/backend/api/models/copy_job.py
@@ -10,7 +10,15 @@ class CopyJob(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     created_at = db.Column(db.DateTime, default=datetime.datetime.now)
+    src_cloud = db.Column(db.String)
+    src_resource = db.Column(db.String)
+    dst_cloud = db.Column(db.String)
+    dst_path = db.Column(db.String)
+    owner = db.Column(db.String)
+
+    status = db.Column(db.String, nullable=True)
+    completed_at = db.Column(db.DateTime, nullable=True)
 
     def __repr__(self):
-        return "<Copy Job '{}'>".format(self.id)
+        return "<Copy Job {}>".format(self.id)
 
