8a37cc42e8879abd8786bf97b54ffaafb9821e35
Andrei Cioara <andrei@cioara.me>
2019-05-30 11:35:18 +0300

    No more dummy jobs

--- src/frontend/js/reducers/apiReducer.jsx
+++ src/frontend/js/reducers/apiReducer.jsx
@@ -1,49 +1,6 @@
 import * as api from 'actions/apiActions.jsx';
 
 
-const DUMMY_JOBS = [
-    {
-        'id': 10,
-        'description': 'Copy Job number 10',
-        'start_time': new Date(new Date() - 100000000),
-        'finish_time': null,
-        'from_uri': 'localhost:~/tmp',
-        'to_uri': 'localhost:~/dev/usr/bin',
-        'status': 'PROGRESS',
-        'progress': 45,
-    },
-    {
-        'id': 11,
-        'description': 'Copy Job number 11',
-        'start_time': new Date(new Date() - 100000000),
-        'finish_time': null,
-        'from_uri': 'localhost:~/tmp',
-        'to_uri': 'localhost:~/dev/usr/bin',
-        'status': 'PROGRESS',
-        'progress': 60,
-    },
-    {
-        'id': 12,
-        'description': 'Copy Job number 12',
-        'start_time': new Date(new Date() - 100000000),
-        'finish_time': null,
-        'from_uri': 'localhost:~/tmp',
-        'to_uri': 'localhost:~/dev/usr/bin',
-        'status': 'PROGRESS',
-        'progress': 23,
-    },
-    {
-        'id': 13,
-        'description': 'Copy Job number 13',
-        'start_time': new Date(new Date() - 100000000),
-        'finish_time': new Date(),
-        'from_uri': 'localhost:~/tmp',
-        'to_uri': 'localhost:~/dev/usr/bin',
-        'status': 'FINISHED',
-        'progress': 100,
-    },
-]
-
 const initialState = {
     clouds: [],
     jobs: [],
