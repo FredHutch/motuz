ece2be4d395ced385cb251e4bb3f383f51b24db4
Andrei Cioara <andrei@cioara.me>
2019-06-03 14:20:15 +0300

    Added serialize form and action for the connections

--- src/frontend/js/views/Dialog/CloudConnectionDialog.jsx
+++ src/frontend/js/views/Dialog/CloudConnectionDialog.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { Modal, Button } from 'react-bootstrap'
 
+import serializeForm from 'utils/serializeForm.jsx';
+
+const CONNECTION_TYPES = [
+    'S3',
+]
+
+
 class CloudConnectionDialog extends React.Component {
     constructor(props) {
         super(props);
+        this.formRef = React.createRef();
     }
 
     render() {
@@ -20,67 +28,97 @@ class CloudConnectionDialog extends React.Component {
                         <Modal.Title>New Cloud Connection</Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
-                        <div className="container">
-                            <h5 className="text-primary mb-2">Details</h5>
+                        <form
+                            action="#"
+                            onSubmit={event => evnet.preventDefault}
+                            ref={this.formRef}
+                        >
+                            <div className="container">
+                                <h5 className="text-primary mb-2">Details</h5>
 
-                            <div className="row form-group">
-                                <div className="col-4 text-right">
-                                    <b>Type</b>
+                                <div className="row form-group">
+                                    <div className="col-4 text-right">
+                                        <b>Type</b>
+                                    </div>
+                                    <div className="col-8">
+                                        <select
+                                            className="form-control"
+                                            name="type"
+                                        >
+                                            {CONNECTION_TYPES.map(d => <option key={d} value={d}>{d}</option>)}
+                                        </select>
+                                    </div>
                                 </div>
-                                <div className="col-8">
-                                    <input type="text" className='form-control'/>
+
+                                <div className="row form-group">
+                                    <div className="col-4 text-right">
+                                        <b>Name</b>
+                                    </div>
+                                    <div className="col-8">
+                                        <input
+                                            type="text"
+                                            className='form-control'
+                                            name='name'
+                                        />
+                                    </div>
+                                </div>
+
+                                <div className="row form-group">
+                                    <div className="col-4 text-right">
+                                        <b>Bucket</b>
+                                    </div>
+                                    <div className="col-8">
+                                        <input
+                                            type="text"
+                                            className='form-control'
+                                            name='bucket'
+                                        />
+                                    </div>
+                                </div>
+
+                                <div className="row form-group">
+                                    <div className="col-4 text-right">
+                                        <b>Region</b>
+                                    </div>
+                                    <div className="col-8">
+                                        <input
+                                            type="text"
+                                            className='form-control'
+                                            name='region'
+                                        />
+                                    </div>
+                                </div>
+
+
+                                <h5 className='text-primary mt-5 mb-2'>Credentials</h5>
+
+                                <div className="row form-group">
+                                    <div className="col-4 text-right">
+                                        <b>access_key_id</b>
+                                    </div>
+                                    <div className="col-8">
+                                        <input
+                                            type="text"
+                                            className='form-control'
+                                            name='access_key_id'
+                                        />
+                                    </div>
+                                </div>
+
+                                <div className="row form-group">
+                                    <div className="col-4 text-right">
+                                        <b>access_key_secret</b>
+                                    </div>
+                                    <div className="col-8">
+                                        <input
+                                            type="text"
+                                            className='form-control'
+                                            name='access_key_secret'
+                                        />
+                                    </div>
                                 </div>
                             </div>
-
-                            <div className="row form-group">
-                                <div className="col-4 text-right">
-                                    <b>Name</b>
-                                </div>
-                                <div className="col-8">
-                                    <input type="text" className='form-control'/>
-                                </div>
-                            </div>
-
-                            <div className="row form-group">
-                                <div className="col-4 text-right">
-                                    <b>Bucket</b>
-                                </div>
-                                <div className="col-8">
-                                    <input type="text" className='form-control'/>
-                                </div>
-                            </div>
-
-                            <div className="row form-group">
-                                <div className="col-4 text-right">
-                                    <b>Region</b>
-                                </div>
-                                <div className="col-8">
-                                    <input type="text" className='form-control'/>
-                                </div>
-                            </div>
-
-
-                            <h5 className='text-primary mt-5 mb-2'>Credentials</h5>
-
-                            <div className="row form-group">
-                                <div className="col-4 text-right">
-                                    <b>access_key_id</b>
-                                </div>
-                                <div className="col-8">
-                                    <input type="text" className='form-control'/>
-                                </div>
-                            </div>
-
-                            <div className="row form-group">
-                                <div className="col-4 text-right">
-                                    <b>access_key_secret</b>
-                                </div>
-                                <div className="col-8">
-                                    <input type="text" className='form-control'/>
-                                </div>
-                            </div>
-
-                        </div>
+                        </form>
                     </Modal.Body>
                     <Modal.Footer>
                         <Button variant="secondary" onClick={() => this.handleClose()}>
@@ -100,16 +138,9 @@ class CloudConnectionDialog extends React.Component {
     }
 
     handleSubmit() {
-        const formData = this.props.data;
+        const form = this.formRef.current;
+        const data = serializeForm(form)
 
-        const data = {
-            "description": `Task ${Math.floor(Math.random() * 100)}`,
-            "src_cloud": formData['source_cloud'].name,
-            "src_resource": formData['source_resource'],
-            "dst_cloud": formData['destination_cloud'].name,
-            "dst_path": formData['destination_path'],
-            "owner": "owner"
-        }
         this.props.onSubmit(data);
     }
 
@@ -126,7 +157,7 @@ CloudConnectionDialog.defaultProps = {
 
 import {connect} from 'react-redux';
 import {hideCloudConnectionDialog} from 'actions/dialogActions.jsx'
-import {createCopyJob} from 'actions/apiActions.jsx'
+import {createCloudConnection} from 'actions/apiActions.jsx'
 
 const mapStateToProps = state => ({
     data: state.dialog.copyJobDialogData,
@@ -134,7 +165,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
     onClose: () => dispatch(hideCloudConnectionDialog()),
-    onSubmit: data => dispatch(createCopyJob(data)),
+    onSubmit: data => dispatch(createCloudConnection(data)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CloudConnectionDialog);
