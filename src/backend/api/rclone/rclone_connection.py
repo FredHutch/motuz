17398781652397940bbe4c01579a6648963758b3
Andrei Cioara <andrei@cioara.me>
2019-06-13 14:08:56 +0300

    The UI is now using rclone to copy files (#33)
    * Added rclone wrapper sandbox
    
    * Implemented ls in the rclone wrapper
    
    * The rclone wrapper now reports progress
    
    * Implemented copy_stop
    
    * Moved rclone into backend
    
    * Deleted celery_app
    
    * Moved rclone inside api
    
    * Copying works
    
    * Updating the progress for celery tasks

---
+++ src/backend/api/rclone/rclone_connection.py
@@ -0,0 +1,159 @@
+import logging
+import subprocess
+import threading
+
+
+class RcloneConnection:
+    def __init__(self, type, region, access_key_id, secret_access_key):
+        self.type = type
+        self.region = region
+        self.access_key_id = access_key_id
+        self.secret_access_key = secret_access_key
+
+        self._job_status = {} # Mapping from id to status string
+        self._stop_events = {} # Mapping from id to threading.Event
+        self._latest_job_id = 0
+
+
+    def ls(self, path):
+        command = (
+            'RCLONE_CONFIG_CURRENT_TYPE={type} '
+            'RCLONE_CONFIG_CURRENT_REGION={region} '
+            'RCLONE_CONFIG_CURRENT_ACCESS_KEY_ID={access_key_id} '
+            'RCLONE_CONFIG_CURRENT_SECRET_ACCESS_KEY={secret_access_key} '
+            'rclone lsjson current:{path}'
+        ).format(
+            type=self.type,
+            region=self.region,
+            access_key_id=self.access_key_id,
+            secret_access_key=self.secret_access_key,
+            path=path,
+        )
+
+        result = self._execute(command)
+        return result
+
+
+    def copy(self, src, dst):
+        command = (
+            'RCLONE_CONFIG_CURRENT_TYPE={type} '
+            'RCLONE_CONFIG_CURRENT_REGION={region} '
+            'RCLONE_CONFIG_CURRENT_ACCESS_KEY_ID={access_key_id} '
+            'RCLONE_CONFIG_CURRENT_SECRET_ACCESS_KEY={secret_access_key} '
+            'rclone copy {src} current:{dst} '
+            '--stats-one-line --progress '
+            '--stats 2s '
+        ).format(
+            type=self.type,
+            region=self.region,
+            access_key_id=self.access_key_id,
+            secret_access_key=self.secret_access_key,
+            src=src,
+            dst=dst,
+        )
+
+        logging.info(command)
+
+        job_id = self.get_next_job_id()
+        self._job_status[job_id] = ''
+        self._stop_events[job_id] = threading.Event()
+        self._execute_interactive(command, job_id)
+        return job_id
+
+
+    def copy_status(self, job_id):
+        return self._job_status[job_id]
+
+
+    def copy_stop(self, job_id):
+        self._stop_events[job_id].set()
+
+    def copy_finished(self, job_id):
+        return self._stop_events[job_id].is_set()
+
+
+    def get_next_job_id(self):
+        self._latest_job_id += 1
+        return self._latest_job_id
+
+
+    def _execute(self, command):
+        byteOutput = subprocess.check_output(command, shell=True)
+        output = byteOutput.decode('UTF-8').rstrip()
+        return output
+
+    def _execute_interactive(self, command, job_id):
+        thread = threading.Thread(target=self.__execute_interactive, kwargs={
+            'command': command,
+            'job_id': job_id,
+        })
+        thread.daemon = True
+        thread.start()
+
+
+    def __execute_interactive(self, command, job_id):
+        stop_event = self._stop_events[job_id]
+
+        process = subprocess.Popen(
+            command,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            shell=True,
+        )
+
+        first_start_sequence = chr(27)
+        start_sequence = ''.join(chr(d) for d in (91, 50, 75, 27, 91, 48, 71))
+
+        start = process.stdout.read(1).decode('utf-8')
+        if not start == first_start_sequence:
+            logging.error("Start character chr({}) is not as expected chr({}) ".format(
+                ord(start), ord(first_start_sequence))
+            )
+            stop_event.set()
+
+        while not stop_event.is_set():
+            start = process.stdout.read(len(start_sequence)).decode('utf-8')
+            if not start == start_sequence:
+                logging.error("Start sequence chr({}) is not as expected chr({}) ".format(
+                    map(ord, start), map(ord, first_start_sequence)
+                ))
+                stop_event.set()
+                continue
+
+            line = ''
+            while True:
+                cc = process.stdout.read(1).decode("utf-8")
+                if cc == first_start_sequence:
+                    break
+                line += cc
+            self._job_status[job_id] = line
+
+        stop_event.set()
+
+
+
+def main():
+    import time
+    import os
+
+    conection = RcloneConnection(
+        type='s3',
+        region=os.environ['MOTUZ_REGION'],
+        access_key_id=os.environ['MOTUZ_ACCESS_KEY_ID'],
+        secret_access_key=os.environ['MOTUZ_SECRET_ACCESS_KEY'],
+    )
+
+    # result = conection.ls('/fh-ctr-mofuz-test/hello/world')
+    job_id = conection.copy('/tmp/motuz/blob2.bin', '/fh-ctr-mofuz-test/hello/world')
+    print("Now sleeping")
+
+    time.sleep(10)
+    print(connection.copy_status(job_id))
+    connection.copy_stop(job_id)
+
+    time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
--- src/backend/api/tasks/celery_tasks.py
+++ src/backend/api/tasks/celery_tasks.py
@@ -1,11 +1,13 @@
 import time
 import random
 import logging
+import re
 
 from .. import celery
-from ..models import CopyJob
+from ..models import CopyJob, CloudConnection
 from ..application import db
 
+from ..rclone.rclone_connection import RcloneConnection
 
 
 @celery.task(name='motuz.api.tasks.my_sleep')
@@ -17,6 +19,46 @@ def my_sleep(message, seconds=1):
 
 @celery.task(name='motuz.api.tasks.copy_job', bind=True)
 def copy_job(self, task_id=None):
+    copy_job = CopyJob.query.get(task_id)
+    copy_job.progress_state = 'PROGRESS'
+    db.session.commit()
+
+    # TODO: make the correct relationship
+    cloud_connections = CloudConnection.query.all()
+    if len(cloud_connections) == 0:
+        copy_job.progress_state = 'FINISHED'
+        copy_job.progress_current = 100
+        copy_job.progress_total = 100
+        return {}
+
+    cloud_connection = cloud_connections[0]
+
+    connection = RcloneConnection(
+        type=cloud_connection.type,
+        region=cloud_connection.region,
+        access_key_id=cloud_connection.access_key_id,
+        secret_access_key=cloud_connection.access_key_secret,
+    )
+
+    job_id = connection.copy(
+        src=copy_job.src_resource,
+        dst=copy_job.dst_path,
+    )
+
+    while not connection.copy_finished(job_id):
+        status = connection.copy_status(job_id)
+        logging.info(status)
+        progress_match = re.search(r'(\d*)%', status)
+        if progress_match is not None:
+            copy_job.progress_current = int(progress_match.group(1))
+            db.session.commit()
+        time.sleep(1)
+
+    return {}
+
+
+@celery.task(name='motuz.api.tasks.dummy_copy_job', bind=True)
+def dummy_copy_job(self, task_id=None):
     copy_job = CopyJob.query.get(task_id)
     copy_job.progress_state = 'PROGRESS'
     db.session.commit()
