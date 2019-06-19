32239abfd61c7665e38bcc78bac62383cc749a0e
Andrei Cioara <andrei@cioara.me>
2019-06-20 02:18:00 +0300

    Added prod scripts

---
+++ bin/prod/backend_start.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+THIS_DIR=$(dirname "$0")
+cd ${THIS_DIR}
+cd ../..
+
+source venv/bin/activate
+
+cd src/backend
+
+uwsgi --ini wsgi.ini
---
+++ bin/prod/rabbitmq_start.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+THIS_DIR=$(dirname "$0")
+cd ${THIS_DIR}
+cd ../..
+
+sudo docker-compose run rabbitmq
