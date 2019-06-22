c213b869bc5237874e297a66088f202dee56e55f
Andrei Cioara <andrei@cioara.me>
2019-06-22 14:43:52 +0300

    Migrations work (#41)

--- bin/database_start.sh
+++ bin/database_start.sh
@@ -6,6 +6,12 @@ THIS_DIR=$(dirname "$0")
 cd ${THIS_DIR}
 cd ..
 
+
+# In order to check that it all works well
+# docker run -it --entrypoint='bash' --net='host' postgres:11.3
+# psql -h 0.0.0.0 -U postgres
+# password: docker
+
 docker run --rm \
     --name local_motuz_database \
     -e POSTGRES_PASSWORD=docker \
