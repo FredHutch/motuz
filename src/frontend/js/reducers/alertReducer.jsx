3e639adfc7ce02f01e0987c2c2b694dee0700fb8
Andrei Cioara <andrei@cioara.me>
2019-07-01 20:07:55 -0700

    Implemented alert modals

---
+++ src/frontend/js/reducers/alertReducer.jsx
@@ -0,0 +1,41 @@
+import * as auth from 'actions/authActions.jsx';
+import * as api from 'actions/apiActions.jsx';
+import * as alert from 'actions/alertActions.jsx';
+
+const initialState = {
+    show: false,
+    text: '',
+};
+
+
+export default (state=initialState, action) => {
+    switch(action.type) {
+    case alert.HIDE_ALERT: {
+        return initialState;
+    }
+
+    case auth.LOGIN_FAILURE:
+    case auth.TOKEN_FAILURE:
+
+    case api.LIST_FILES_FAILURE:
+    case api.LIST_COPY_JOBS_FAILURE:
+    case api.RETRIEVE_COPY_JOB_FAILURE:
+    case api.CREATE_COPY_JOB_FAILURE:
+    case api.STOP_COPY_JOB_FAILURE:
+    case api.LIST_CLOUD_CONNECTIONS_FAILURE:
+    case api.CREATE_CLOUD_CONNECTION_FAILURE:
+    case api.UPDATE_CLOUD_CONNECTION_FAILURE:
+    case api.DELETE_CLOUD_CONNECTION_FAILURE:
+
+    {
+        return {
+            ...state,
+            show: true,
+            text: action.payload.response || action.payload,
+        };
+    }
+
+    default:
+        return state;
+    }
+};
