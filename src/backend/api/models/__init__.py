21b52ea2e9ac648e611e8ceadc3631401a7a9950
Andrei Cioara <andrei@cioara.me>
2019-07-02 06:51:52 -0700

    Moved cloud connection serializer inside the model

--- src/backend/api/models/__init__.py
+++ src/backend/api/models/__init__.py
@@ -1,3 +1,3 @@
 from .invalid_token import InvalidToken
 from .copy_job import CopyJob, CopyJobSerializer
-from .cloud_connection import CloudConnection
+from .cloud_connection import CloudConnection, CloudConnectionSerializer
