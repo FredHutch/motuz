a77500fd086801a715a1be71f0d1b975d27601d4
Andrei Cioara <andrei@cioara.me>
2019-07-03 17:23:00 -0700

    Added all fields for swift, azure, gcp and s3 compatible connections

--- src/backend/api/models/cloud_connection.py
+++ src/backend/api/models/cloud_connection.py
@@ -15,13 +15,38 @@ class CloudConnection(db.Model):
 
     type = db.Column(db.String)
 
-    s3_bucket = db.Column(db.String)
-    s3_region = db.Column(db.String)
-    s3_access_key_id = db.Column(db.String)
-    s3_secret_access_key = db.Column(db.String)
+    # S3 fields
+    s3_bucket = db.Column(db.String, nullable=True)
+    s3_access_key_id = db.Column(db.String, nullable=True)
+    s3_secret_access_key = db.Column(db.String, nullable=True)
+    s3_region = db.Column(db.String, nullable=True)
+
+    # S3 compatible fields
+    s3_endpoint = db.Column(db.String, nullable=True)
+    s3_v2_auth = db.Column(db.String, nullable=True)
+
+    # Azure fields
+    azure_account = db.Column(db.String, nullable=True)
+    azure_key = db.Column(db.String, nullable=True)
+
+    # Swift V1 fields
+    swift_user = db.Column(db.String, nullable=True)
+    swift_key = db.Column(db.String, nullable=True)
+    swift_auth = db.Column(db.String, nullable=True)
+
+    # Swift V2 additional fields
+    swift_tenant = db.Column(db.String)
+
+    # Google Cloud Services fields
+    gcp_client_id = db.Column(db.String, nullable=True)
+    gcp_service_account_credentials = db.Column(db.String, nullable=True)
+    gcp_project_number = db.Column(db.String, nullable=True)
+    gcp_object_acl = db.Column(db.String, nullable=True)
+    gcp_bucket_acl = db.Column(db.String, nullable=True)
+
 
     created_at = db.Column(db.DateTime, default=datetime.datetime.now)
 
 
     def __repr__(self):
-        return "<Cloud Connection {}>".format(self.name)
+        return "<Cloud Connection {} ({})>".format(self.name, self.type)
