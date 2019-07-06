3d8bc65aee659b489543177bfbc54b92967a7cff
Andrei Cioara <andrei@cioara.me>
2019-07-05 19:54:49 -0700

    Finished refactoring CopyJobTable

--- src/frontend/js/views/App/CopyJobSection/CopyJobTable.jsx
+++ src/frontend/js/views/App/CopyJobSection/CopyJobTable.jsx
@@ -30,45 +30,35 @@ class CopyJobTable extends React.Component {
             );
         })
 
-
-        // TODO: Optimize
-        const jobs = this.props.jobs.map(job => {
-            const src_cloud_id = job['src_cloud_id']
-            if (!src_cloud_id) {
-                job['src_cloud'] = 'file'
-            } else {
-                const src_cloud = this.props.connections.find(d => d.id === src_cloud_id)
-                if (src_cloud) {
-                    job['src_cloud'] = src_cloud.type
-                }
+        const cloudMapping = {
+            0: {
+                type: 'file',
             }
-
-            const dst_cloud_id = job['dst_cloud_id']
-            if (!dst_cloud_id) {
-                job['dst_cloud'] = 'file'
-            } else {
-                const dst_cloud = this.props.connections.find(d => d.id === dst_cloud_id)
-                if (dst_cloud) {
-                    job['dst_cloud'] = dst_cloud.type
-                }
-            }
-
-            return job
+        }
+        this.props.connections.forEach(connection => {
+            cloudMapping[connection.id] = connection
         })
 
-        const tableRows = jobs.map((job, i) => {
+
+        const tableRows = this.props.jobs.map((job, i) => {
             const progress = Math.round(job.progress_current / job.progress_total * 100);
+
+            const src_cloud_id = job['src_cloud_id'] || 0
+            const src_cloud = cloudMapping[src_cloud_id]
+
+            const dst_cloud_id = job['dst_cloud_id'] || 0
+            const dst_cloud = cloudMapping[dst_cloud_id]
 
             const source = (
                 <React.Fragment>
-                    <b>{job.src_cloud}</b>
+                    <b>{src_cloud.type}</b>
                     <span>://</span>
                     <span>{job.src_resource}</span>
                 </React.Fragment>
             )
             const destination = (
                 <React.Fragment>
-                    <b>{job.dst_cloud}</b>
+                    <b>{dst_cloud.type}</b>
                     <span>://</span>
                     <span>{job.dst_path}</span>
                 </React.Fragment>
@@ -82,6 +72,7 @@ class CopyJobTable extends React.Component {
                 source,
                 destination,
             }
+
             return (
                 <tr
                     onClick={event => this._onSelectJob(job)}
@@ -110,7 +101,7 @@ class CopyJobTable extends React.Component {
             );
         })
 
-        const currentJobsInProgress = new Set(jobs
+        const currentJobsInProgress = new Set(this.props.jobs
             .filter(d => d.progress_state === 'PROGRESS')
             .map(d => d.id)
         )
