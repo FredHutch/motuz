00223f74a38a2b2de801eb4dac367e0586bd55fb
Andrei Cioara <andrei@cioara.me>
2019-07-08 21:00:50 -0700

    Improved robustness of celery tasks

--- src/backend/api/tasks/celery_tasks.py
+++ src/backend/api/tasks/celery_tasks.py
@@ -13,51 +13,75 @@ from ..utils.rclone_connection import RcloneConnection
 
 @celery.task(name='motuz.api.tasks.copy_job', bind=True)
 def copy_job(self, task_id=None):
-    start_time = time.time()
+    try:
+        start_time = time.time()
 
-    copy_job = CopyJob.query.get(task_id)
-    copy_job.progress_state = 'PROGRESS'
-    db.session.commit()
+        copy_job = CopyJob.query.get(task_id)
+        copy_job.progress_state = 'PROGRESS'
+        db.session.commit()
 
-    connection = RcloneConnection()
-    connection.copy(
-        src_data=copy_job.src_cloud,
-        src_path=copy_job.src_resource,
-        dst_data=copy_job.dst_cloud,
-        dst_path=copy_job.dst_path,
-        user=copy_job.owner,
-        job_id=task_id,
-    )
+        connection = RcloneConnection()
+        connection.copy(
+            src_data=copy_job.src_cloud,
+            src_path=copy_job.src_resource,
+            dst_data=copy_job.dst_cloud,
+            dst_path=copy_job.dst_path,
+            user=copy_job.owner,
+            job_id=task_id,
+        )
 
-    while not connection.copy_finished(task_id):
-        progress_current = connection.copy_percent(task_id)
-        copy_job.progress_current = progress_current
+        while not connection.copy_finished(task_id):
+            progress_current = connection.copy_percent(task_id)
+            copy_job.progress_current = progress_current
+            copy_job.progress_execution_time = int(time.time() - start_time)
+            db.session.commit()
+
+            self.update_state(state='PROGRESS', meta={
+                'text': connection.copy_text(task_id),
+                'error_text': connection.copy_error_text(task_id)
+            })
+
+            time.sleep(1)
+
+
+        exitstatus = connection.copy_exitstatus(task_id)
+        if exitstatus == -1:
+            logging.error("Copy Job did not set its status")
+            copy_job.progress_state = 'UNSET'
+        elif exitstatus == 0:
+            copy_job.progress_state = 'SUCCESS'
+        else:
+            copy_job.progress_state = 'FAILED'
+
+
+        copy_job.progress_current = 100
         copy_job.progress_execution_time = int(time.time() - start_time)
         db.session.commit()
 
-        self.update_state(state='PROGRESS', meta={
+        return {
             'text': connection.copy_text(task_id),
             'error_text': connection.copy_error_text(task_id)
-        })
+        }
+    except Exception as e:
+        logging.exception(e)
 
-        time.sleep(1)
+        try:
+            copy_job.progress_current = 100
+            copy_job.progress_state = 'FAILED'
+        except:
+            pass
 
+        try:
+            copy_job.progress_execution_time = int(time.time() - start_time)
+        except:
+            pass
 
-    exitstatus = connection.copy_exitstatus(task_id)
-    if exitstatus == -1:
-        logging.error("Copy Job did not set its status")
-        copy_job.progress_state = 'UNSET'
-    elif exitstatus == 0:
-        copy_job.progress_state = 'SUCCESS'
-    else:
-        copy_job.progress_state = 'FAILED'
+        try:
+            db.session.commit()
+        except:
+            pass
 
-
-    copy_job.progress_current = 100
-    copy_job.progress_execution_time = int(time.time() - start_time)
-    db.session.commit()
-
-    return {
-        'text': connection.copy_text(task_id),
-        'error_text': connection.copy_error_text(task_id)
-    }
+        return {
+            'text': '',
+            'error_text': str(e),
+        }
