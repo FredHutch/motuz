61f2fb62dd516188bb2b043470b579f162c4982e
Dan Tenenbaum <dtenenba@fhcrc.org>
2019-07-12 14:28:48 -0700

    cleanup

--- bin/prod/update.sh
+++ bin/prod/update.sh
@@ -9,10 +9,9 @@ else
 fi
 
 
-sudo bash -c "(cd /root/motuz && git pull)" # # TODO unhardcode motuz dir
 echo updating code...
+sudo bash -c "(cd /root/motuz && git pull)" # # TODO unhardcode motuz dir
 # TODO - make sure we are in the right branch?
-git pull
 echo running redeploy script...
 sudo bash -c "(cd /root/motuz && bin/prod/redeploy.sh)"
 echo Done.
