cd61b6070ef7c576b00c98a0a65c43e41051a642
Andrei Cioara <andrei@cioara.me>
2019-07-11 22:06:18 -0700

    Database initialization works

--- docker/database_init/database_init.sh
+++ docker/database_init/database_init.sh
@@ -2,4 +2,56 @@
 
 set -e
 
-echo "HERE"
+
+readonly REQUIRED_ENV_VARS=(
+  "DB_USER"
+  "DB_PASSWORD"
+  "DB_DATABASE"
+  "POSTGRES_USER"
+  "POSTGRES_PASSWORD"
+)
+
+
+init() {
+  check_env_vars_set
+  init_user_and_db
+}
+
+
+# Checks if all of the required environment
+# variables are set. If one of them isn't,
+# echoes a text explaining which one isn't
+# and the name of the ones that need to be
+check_env_vars_set() {
+  for required_env_var in ${REQUIRED_ENV_VARS[@]}; do
+    if [[ -z "${!required_env_var}" ]]; then
+      echo "Error:
+    Environment variable '$required_env_var' not set.
+    Make sure you have the following environment variables set:
+      ${REQUIRED_ENV_VARS[@]}
+Aborting."
+      exit 1
+    fi
+  done
+}
+
+
+# Performs the initialization in the already-started PostgreSQL
+# using the preconfigured POSTGRE_USER user.
+init_user_and_db() {
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+     CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD';
+     CREATE DATABASE $DB_DATABASE;
+     GRANT ALL PRIVILEGES ON DATABASE $DB_DATABASE TO $DB_USER;
+EOSQL
+}
+
+
+
+
+docker-entrypoint.sh postgres &
+
+echo "Waiting for DB to start..."
+sleep 5
+
+init "$@"
