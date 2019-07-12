14dd16235555b8d50e99d0d343a600be1a075952
Dan Tenenbaum <dtenenba@fhcrc.org>
2019-07-12 14:18:52 -0700

    re-org script to use sudo bash

--- bin/prod/update.sh
+++ bin/prod/update.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 if ps aux| grep -v grep |grep --quiet rclone; then
-  echo rclone is running, not redeploying at this time
-  echo in future, consider adding delays/retries here
+  echo "rclone is running, not redeploying at this time"
+  echo "in future, consider adding delays/retries here"
   exit 1
 else
   echo rclone is not running, ok to deploy
 fi
 
 
-sudo -s
-cd ~root/motuz # # TODO unhardcode motuz dir
+sudo bash -c "(cd /root/motuz && git pull)" # # TODO unhardcode motuz dir
 echo updating code...
+# TODO - make sure we are in the right branch?
 git pull
 echo running redeploy script...
-bin/prod/redeploy.sh
+sudo bash -c "(cd /root/motuz && /bin/prod/redeploy.sh)"
+echo Done.
+
 
 
