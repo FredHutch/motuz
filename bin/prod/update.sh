0ea9fb683ac859a02d29deadd209d0d8f12ed974
Dan Tenenbaum <dtenenba@fhcrc.org>
2019-07-12 14:35:01 -0700

    set to work on master (when merged)

--- bin/prod/update.sh
+++ bin/prod/update.sh
@@ -13,6 +13,7 @@ echo updating code...
 sudo bash -c "(cd /root/motuz && git pull)" # # TODO unhardcode motuz dir
 # TODO - make sure we are in the right branch?
 echo running redeploy script...
+# TODO - disable 'sleep' in redeploy script if calling from here, to minimize downtime
 sudo bash -c "(cd /root/motuz && bin/prod/redeploy.sh)"
 echo Done.
 
