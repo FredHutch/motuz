f7867e86df9cdfd50d41d675a47d9b132dda1d75
Andrei Cioara <andrei@cioara.me>
2019-07-14 16:56:22 -0700

    Finalized start script

--- bin/start.sh
+++ bin/start.sh
@@ -7,8 +7,7 @@ cd ${THIS_DIR}
 cd ..
 
 # Initialize Database
-docker build --no-cache=true -t motuz_database_init:latest -f docker/database_init/Dockerfile .
-docker run -it --net='host' -v /docker/volumes/postgres:/var/lib/postgresql/data motuz_database_init:latest
+./bin/database_install.sh
 
 # Start the application
 docker-compose up -d
