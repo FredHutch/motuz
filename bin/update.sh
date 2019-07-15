6e531ff0601fe0c19e7fe29f3daf15010b645a32
Dan Tenenbaum <dtenenba@fhcrc.org>
2019-07-15 11:44:14 -0700

    Add CI/CD (#145)
    * first attempt
    
    * quote env var
    
    * use echo not cat
    
    * diagnostics
    
    * pull ssh key from S3 instead of env var
    
    * fix wrong filename
    
    * change ssh command
    
    * re-org script to use sudo bash
    
    * fix typo
    
    * cleanup
    
    * set to work on master (when merged)
    
    * refactor update (pre-redeploy) script to reduce the chance of race conditions
    
    * clarifying comments

---
+++ bin/update.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# This script is part of the CI/CD pipeline in use 
+# at Fred Hutch to deploy Motuz. It may not be super useful
+# in other deployments.
+
+
+set -e
+set -o pipefail
+
+# clone the repo, capturing the latest changes.
+sudo bash -c "(cd /root &&  rm -rf motuz-temp  &&  git clone https://github.com/FredHutch/motuz.git motuz-temp)"
+
+# check if rclone is running
+if pgrep -l rclone|grep -q rclone; then
+  echo "rclone is running, not redeploying at this time"
+  echo "in future, consider adding delays/retries here"
+  exit 1
+else
+  echo rclone is not running, ok to redeploy
+fi
+
+
+
+
+echo updating code...
+sudo bash -c "(cd /root && rm -rf motuz.bak && mv motuz motuz.bak && mv motuz-temp motuz)"
+
+
+# TODO - make sure we are in the right branch?
+echo running redeploy script...
+# TODO - disable 'sleep' in redeploy script if calling from here, to minimize downtime
+sudo bash -c "(cd /root/motuz && bin/redeploy.sh)"
+
+# finally clean up and remove backup dir:
+sudo bash -c "(cd /root && rm -rf motuz-bak)"
+
+echo Done.
+
+
+
