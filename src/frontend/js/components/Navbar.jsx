fd50b4e0a6032449f4bba5701ddf6d29900f849b
Andrei Cioara <andrei@cioara.me>
2019-12-21 15:13:32 -0800

    Fixed navbar

--- src/frontend/js/components/Navbar.jsx
+++ src/frontend/js/components/Navbar.jsx
@@ -6,17 +6,15 @@ import UserMenu from 'components/UserMenu.jsx'
 export default class Navbar extends React.PureComponent {
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="mr-auto w-25 navbar-nav">
+            <nav className="navbar navbar-expand-sm navbar-light bg-light">
+                <div className="nav navbar-nav">
                     <Link to='/'>
                         <span className="navbar-brand">Motuz</span>
                     </Link>
                 </div>
-                <div className="navbar-collapse navbar-right collapse order-3 dual-collapse2">
-                    <div className="navbar-nav ml-auto">
-                        <Link to="/clouds" className="nav-link">My Cloud Connections</Link>
-                        <UserMenu />
-                    </div>
+                <div className="nav navbar-nav ml-auto">
+                    <Link to="/clouds" className="nav-link">My Cloud Connections</Link>
+                    <UserMenu />
                 </div>
             </nav>
         );
