5181c404ffcdb3bb02c37c9209b0f776141cf059
Andrei Cioara <andrei@cioara.me>
2020-01-25 10:51:12 -0800

    Added tooltip information for the settings items

---
+++ src/frontend/js/components/Info.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { OverlayTrigger, Popover } from 'react-bootstrap'
+
+import Icon from 'components/Icon.jsx'
+
+class Info extends React.PureComponent {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <OverlayTrigger
+                trigger="focus"
+                placement="bottom"
+                overlay={props => (
+                    <Popover
+                        {...props }
+                        show={props.show.toString()}
+                    >
+                        {this.props.children}
+                    </Popover>
+                )}
+            >
+                <a
+                    className={this.props.className}
+                    href="javascript:void(0);"
+                >
+                    <Icon name='question' verticalAlign='top' />
+                </a>
+            </OverlayTrigger>
+        )
+    }
+}
+
+export default Info;
---
+++ src/frontend/js/components/ToggleInfo.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { OverlayTrigger, Popover } from 'react-bootstrap'
+import Toggle from 'react-toggle'
+
+import Info from 'components/Info.jsx'
+
+class ToggleInfo extends React.PureComponent {
+    render() {
+        return (
+            <Info {...this.props}>
+                <div>
+                    <Toggle checked={true} readOnly={true}/>
+                    <p>
+                        {this.props.on}
+                    </p>
+                </div>
+                <hr/>
+                <div>
+                    <Toggle checked={false} readOnly={true}/>
+                    <p>
+                        {this.props.off}
+                    </p>
+                </div>
+            </Info>
+        )
+    }
+}
+
+ToggleInfo.defaultProps = {
+    on: '',
+    off: '',
+}
+
+export default ToggleInfo;
