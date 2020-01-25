32d02b29e0b1a17a1d705c695bbb3742155d26b3
Andrei Cioara <andrei@cioara.me>
2020-01-25 11:18:05 -0800

    Added more sophisticated pane refresh (#242)
    * Added more sophisticated pane refresh
    
    * Simplified promise.all

--- src/frontend/js/actions/paneActions.jsx
+++ src/frontend/js/actions/paneActions.jsx
@@ -88,22 +88,27 @@ export const refreshPane = (side='left') => {
 
         const path = state.pane.panes[side][0].path;
 
-        await Promise.all([
-            dispatch(directoryChange(side, path)),
-        ]);
+        await dispatch(directoryChange(side, path))
     }
 }
 
-export const refreshPanes = () => {
+export const refreshPanes = (path=null) => {
     return async (dispatch, getState) => {
         const state = getState();
+
+        const promises = []
 
         const pathLeft = state.pane.panes.left[0].path;
         const pathRight = state.pane.panes.right[0].path;
 
-        await Promise.all([
-            dispatch(directoryChange('left', pathLeft)),
-            dispatch(directoryChange('right', pathRight)),
-        ]);
+        if (path == null || path === pathLeft) {
+            promises.push(dispatch(directoryChange('left', pathLeft)))
+        }
+
+        if (path == null || path === pathRight) {
+            promises.push(dispatch(directoryChange('right', pathRight)))
+        }
+
+        await Promise.all(promises);
     }
 }
