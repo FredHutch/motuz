3f9e746b447cdfc8ecb7170374ecb0d4f3a6b3e5
Andrei Cioara <andrei@cioara.me>
2020-02-08 18:17:49 -0800

    Improved load-secrets

--- docker/load-secrets.sh
+++ docker/load-secrets.sh
@@ -2,9 +2,22 @@
 
 set -e
 
-SECRETS_DIRECTORY='/run/secrets'
+if [ -z "$1" ]; then
+    SECRETS_DIRECTORY="/run/secrets"
+else
+    SECRETS_DIRECTORY="$1"
+fi
 
-for secret_path in $SECRETS_DIRECTORY/*; do
-    secret=$(basename "$secret_path")
-    eval "export $secret=\"$(cat $secret_path)\""
+if [ -z "$(ls -A $SECRETS_DIRECTORY 2> /dev/null)" ]; then
+    echo "No secrets found at $SECRETS_DIRECTORY"
+    exit 0
+fi
+
+
+echo $SECRETS_DIRECTORY
+
+for secret_path in "$SECRETS_DIRECTORY"/*; do
+    key=$(basename "$secret_path")
+    value=$(cat $secret_path)
+    eval "export $key=\"$value\""
 done
