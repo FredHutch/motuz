de581a3fada2915ded6d861c679b5f584d669bb7
Andrei Cioara <andrei@cioara.me>
2020-03-11 19:49:10 -0700

    Finalized the email package

--- src/backend/api/utils/email_utils.py
+++ src/backend/api/utils/email_utils.py
@@ -1,6 +1,7 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from smtplib import SMTP
+from email.header import Header
+import smtplib
 
 class Email:
     def __init__(self, *,
@@ -9,7 +10,6 @@ class Email:
         MAIL_PASSWORD=None,
         MAIL_DEFAULT_SENDER=None,
         MAIL_USE_TLS=False,
-        MAIL_USE_SSL=False,
         MAIL_DEBUG=False,
     ):
         self._MAIL_SERVER = MAIL_SERVER
@@ -17,33 +17,47 @@ class Email:
         self._MAIL_PASSWORD = MAIL_PASSWORD
         self._MAIL_DEFAULT_SENDER = MAIL_DEFAULT_SENDER
         self._MAIL_USE_TLS = MAIL_USE_TLS
-        self._MAIL_USE_SSL = MAIL_USE_SSL
         self._MAIL_DEBUG = MAIL_DEBUG
 
 
     def send(self, *, to, subject, body):
-        with SMTP(self._MAIL_SERVER) as smtp:
-            if self._MAIL_USERNAME and self._MAIL_PASSWORD:
-                smtp.login(self._MAIL_USERNAME, self._MAIL_PASSWORD)
+        try:
+            with smtplib.SMTP(self._MAIL_SERVER) as smtp:
+                if self._MAIL_USERNAME or self._MAIL_PASSWORD:
+                    smtp.login(self._MAIL_USERNAME, self._MAIL_PASSWORD)
 
-            msg = MIMEMultipart()
+                if self._MAIL_USE_TLS:
+                    smtp.starttls()
 
-            # setup the parameters of the message
-            msg['From'] = self._MAIL_DEFAULT_SENDER
-            msg['To'] = to
-            msg['Subject'] = subject
-            msg.attach(MIMEText(body, 'plain'))
+                if self._MAIL_DEBUG:
+                    smtp.set_debuglevel(1)
 
-            result = smtp.send_message(msg)
+                msg = MIMEText(body, 'plain', 'utf-8')
+                msg['Subject'] = Header(subject, 'utf-8')
+                msg['From'] = self._MAIL_DEFAULT_SENDER
+                msg['To'] = to
+
+                result = smtp.send_message(msg)
+        except smtplib.SMTPException as e:
+            raise EmailError("Email send FAILED: {}".format(repr(e)))
+        except Exception as e:
+            raise EmailError("Email sending FAILED {}".format(repr(e)))
+
+
+
+class EmailError(Exception):
+    pass
+
 
 
 if __name__ == "__main__":
     email = Email(
-        MAIL_SERVER='localhost:222',
+        MAIL_SERVER='localhost:25',
         MAIL_DEFAULT_SENDER='noreply@example.org',
+        MAIL_DEBUG=False,
     )
     email.send(
         subject="Test",
-        to='noreply@example.com',
+        to='hello@mail.com',
         body="Hello World"
     )
