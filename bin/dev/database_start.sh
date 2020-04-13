f20a5ef49186d898dfa311063c3a89ff856ca452
Andrei Cioara <andrei@cioara.me>
2020-04-12 20:24:03 -0700

    Added deleted instruction

--- bin/dev/database_start.sh
+++ bin/dev/database_start.sh
@@ -11,6 +11,8 @@ cd ../..
 # docker run -it --entrypoint='bash' --net='host' postgres:11.3
 # psql postgresql://motuz_user:motuz_password@0.0.0.0:5432/motuz
 # Or
+# psql postgresql://postgres:docker@0.0.0.0:5432/postgres
+# Or
 # psql ${MOTUZ_DATABASE_PROTOCOL}://${MOTUZ_DATABASE_USER}:${MOTUZ_DATABASE_PASSWORD}@${MOTUZ_DATABASE_HOST}/${MOTUZ_DATABASE_NAME}
 
 docker run --rm \
