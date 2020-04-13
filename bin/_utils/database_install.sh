5e8ecb4c1393ec5f23ad0a828e03bf9662e0b311
Andrei Cioara <andrei@cioara.me>
2020-04-12 23:07:47 -0700

    Refactored database_install

--- bin/_utils/database_install.sh
+++ bin/_utils/database_install.sh
@@ -6,7 +6,5 @@ THIS_DIR=$(dirname "$0")
 cd ${THIS_DIR}
 cd ../..
 
-composeFileArgs="-f docker-compose.yml -f docker-compose.override.yml"
-
-docker-compose $composeFileArgs -f deployment/docker-compose/docker-compose.build.yml build --no-cache database_init
-docker-compose $composeFileArgs -f deployment/docker-compose/docker-compose.init.yml run database_init
+docker-compose -f docker-compose.yml -f docker-compose.override.yml -f deployment/docker-compose/docker-compose.build.yml build --no-cache database_init
+docker-compose -f docker-compose.yml -f docker-compose.override.yml -f deployment/docker-compose/docker-compose.init.yml run database_init
