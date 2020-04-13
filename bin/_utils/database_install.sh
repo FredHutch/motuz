fe3f33e636b048b2e47cd49154cd8344e6bbd0c1
Andrei Cioara <andrei@cioara.me>
2020-04-12 20:34:56 -0700

    Typo

--- bin/_utils/database_install.sh
+++ bin/_utils/database_install.sh
@@ -9,4 +9,4 @@ cd ../..
 composeFileArgs="-f docker-compose.yml -f docker-compose.override.yml"
 
 docker-compose $composeFileArgs -f deployment/docker-compose/docker-compose.build.yml build --no-cache database_init
-docker-compose composeFileArgs -f deployment/docker-compose/docker-compose.init.yml up database_init
+docker-compose $composeFileArgs -f deployment/docker-compose/docker-compose.init.yml up database_init
