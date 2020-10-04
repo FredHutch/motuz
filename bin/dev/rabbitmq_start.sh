5ecaf72a675d0b3b884047ae058cb03f9a45cc26
Andrei Cioara <andrei@cioara.me>
2020-10-04 14:14:04 -0700

    Fixed dev rabbitmq version

--- bin/dev/rabbitmq_start.sh
+++ bin/dev/rabbitmq_start.sh
@@ -6,4 +6,4 @@ THIS_DIR=$(dirname "$0")
 cd ${THIS_DIR}
 cd ../..
 
-docker run --hostname 0.0.0.0 -p 5672:5672 -p 15672:15672 rabbitmq:3-management
+docker run --hostname 0.0.0.0 -p 5672:5672 -p 15672:15672 rabbitmq:3.7.16-management
